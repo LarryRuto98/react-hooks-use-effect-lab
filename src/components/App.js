51380d4a83839b0f1e6555d7826f95232cda31a7
larryruto98 <larryruto@gmail.com>
2024-12-06 12:10:37 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,19 @@ import quiz from "../data/quiz";
 
 function App() {
   const [questions, setQuestions] = useState(quiz);
-  const [currentQuestionId, setCurrentQuestion] = useState(1);
-  const [score, setScore] = useState(0);
-  const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  const [currentQuestionId, setCurrentQuestion] = useState(1);  
+  const [score, setScore] = useState(0);  
+  const currentQuestion = questions.find((q) => q.id === currentQuestionId);  
 
   function handleQuestionAnswered(correct) {
     if (currentQuestionId < questions.length) {
-      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
+      setCurrentQuestion((prevId) => prevId + 1); 
     } else {
-      setCurrentQuestion(null);
+      setCurrentQuestion(null); 
     }
+
     if (correct) {
-      setScore((score) => score + 1);
+      setScore((prevScore) => prevScore + 1); 
     }
   }
 
@@ -25,12 +26,12 @@ function App() {
         {currentQuestion ? (
           <Question
             question={currentQuestion}
-            onAnswered={handleQuestionAnswered}
+            onAnswered={handleQuestionAnswered} 
           />
         ) : (
           <>
             <h1>Game Over</h1>
-            <h2>Total Correct: {score}</h2>
+            <h2>Total Correct: {score}</h2>  
           </>
         )}
       </section>
